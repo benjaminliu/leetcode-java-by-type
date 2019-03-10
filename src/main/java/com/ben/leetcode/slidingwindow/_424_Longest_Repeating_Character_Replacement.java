1addbfa3539faec9e01805d71d3bedcf435913c4
Benjamin <benjamin.liu@live.cn>
2019-03-10 21:37:53 +0800

    1

--- src/main/java/com/ben/leetcode/slidingwindow/_424_Longest_Repeating_Character_Replacement.java
+++ src/main/java/com/ben/leetcode/slidingwindow/_424_Longest_Repeating_Character_Replacement.java
@@ -1,4 +1,43 @@
 package com.ben.leetcode.slidingwindow;
 
 public class _424_Longest_Repeating_Character_Replacement {
+    // 1.Given a string convert it to a string with all same characters with minimal changes
+    // length of the entire string - number of times of the maximum occurring character in the string
+
+    // 2.we can apply the at most k changes constraint and maintain a sliding window such that
+    // (length of substring - number of times of the maximum occurring character in the substring) <= k
+    static class Solution {
+        public int characterReplacement(String s, int k) {
+            if (s == null || s.length() == 0 || k < 0)
+                return 0;
+
+            int n = s.length();
+            char[] cs = s.toCharArray();
+            int[] map = new int[26];
+            int left = 0, right = 0;
+            int max = 0;
+            while (right < n) {
+                char c = cs[right];
+                right++;
+                map[c - 'A']++;
+                while (!isValid(map, k)) {
+                    c = cs[left];
+                    left++;
+                    map[c - 'A']--;
+                }
+                max = Math.max(max, right - left);
+            }
+            return max;
+        }
+
+        private boolean isValid(int[] map, int k) {
+            int max = 0;
+            int total = 0;
+            for (int i : map) {
+                total += i;
+                max = Math.max(max, i);
+            }
+            return total - max <= k;
+        }
+    }
 }
--- src/main/java/com/ben/leetcode/slidingwindow/_76_Minimum_Window_Substring.java
+++ src/main/java/com/ben/leetcode/slidingwindow/_76_Minimum_Window_Substring.java
@@ -1,4 +1,39 @@
 package com.ben.leetcode.slidingwindow;
 
+/**
+ * need more care
+ */
 public class _76_Minimum_Window_Substring {
+    static class Solution {
+        public String minWindow(String s, String t) {
+            int[] map = new int[128];
+            for (int i = 0; i < t.length(); i++) {
+                map[t.charAt(i)]++;
+            }
+            int counter = t.length();
+            int left = 0, right = 0, window = Integer.MAX_VALUE, head = -1;
+            while (right < s.length()) {
+                char rc = s.charAt(right);
+                right++;
+                if (map[rc] > 0) {
+                    counter--;
+                }
+                map[rc]--;
+
+                while (counter == 0) {
+                    if (right - left < window) {
+                        window = right - left;
+                        head = left;
+                    }
+                    char lc = s.charAt(left);
+                    left++;
+                    if (map[lc] == 0) {
+                        counter++;
+                    }
+                    map[lc]++;
+                }
+            }
+            return head == -1 ? "" : s.substring(head, head + window);
+        }
+    }
 }
